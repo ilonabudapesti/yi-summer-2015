430cbe47074777f6e638e15013911917eb0174eb
Brendan <cyhtan@gmail.com>
2015-05-29 00:22:55 -0400

    Finished save button functionality

--- tracker/js/app.js
+++ tracker/js/app.js
@@ -1,15 +1,17 @@
 $('document').ready( function () {
 
-    var taskRow = $('.task').clone();
+    var taskRow = $('.taskRow').clone();
     
     var taskList = [];
 
-    $('html').on('click', '.btn-save', function (e) {
+    // Save button
+    $('body').on('click', '.btn-save', function (e) {
         
         var savedObject = {};
 
         var $inputParents = $(e.target).parents('tr').children();
 
+        // Get input values
         savedObject.start = $inputParents.children('input:eq(0)').val();
         savedObject.stop = $inputParents.children('input:eq(1)').val();
         savedObject.description = $inputParents.children('input:eq(2)').val();
@@ -20,16 +22,22 @@ $('document').ready( function () {
         console.log(savedObject);
         console.log(taskList);
 
-        $(e.target).parents('tbody').append( taskRow.clone() );
+        // If saving the last row, add a new empty row
+        if ( $(e.target).parents('.taskRow').is(':last-child') ) {
+            $(e.target).parents('tbody').append( taskRow.clone() );
+        }
 
+        // Replace input fields with regular text values
         $inputParents.children('input:first').after(savedObject.start).remove();
         $inputParents.children('input:first').after(savedObject.stop).remove();
         $inputParents.children('input:first').after(savedObject.description).remove();
         $inputParents.children('input:first').after(savedObject.tags).remove();
 
-
+        $(e.target).parents('.buttonTd').children('.btn-save').addClass('hidden');
 
     });
+
+
 
 
 });
