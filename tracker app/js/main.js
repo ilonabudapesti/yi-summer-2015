627c7ab8d24a96f84b812ede5a00640fbd96ae0e
Will Mainwaring <willm@yogainternational.com>
2015-06-02 17:58:12 -0400

    added for loop to increment display

--- tracker app/js/main.js
+++ tracker app/js/main.js
@@ -1,20 +1,25 @@
 var entries = [];
+ var i = 0;
 
  function addEntry() {
  	var entry = {};
 	entry.description = document.getElementById("test1").value;
 	entry.tags = document.getElementById("tags1").value;
 	entry.start = document.getElementById("start").value;
-	entry.end = document.getElementById("end").value;
+	entry.ends = document.getElementById("end").value;
 	entries.push(entry);
-	console.log(entries);
-	var stringifyArray = JSON.stringify(entryArray);
-	var entriesContainer = document.getElementById("entriesContainer");
-	var p = document.createElement ('p');
-	var addArray = document.createTextNode(stringifyArray);
-	p.appendChild(addArray);
-	document.getElementById("entriesContainer").appendChild(p);
+		for (i; i < entries.length; i++) {	
+			var stringifyArray =  
+			entries[i].description + ", " + entries[i].tags+ ", " + entries[i].start + ", " +
+			entries[i].ends
+			var entriesContainer = document.getElementById("entriesContainer");
+			var p = document.createElement ('p');
+			var addArray = document.createTextNode(stringifyArray);
+			p.appendChild(addArray);
+			document.getElementById("entriesContainer").appendChild(p);
 
+		};
+	return i;
 
 }
 
