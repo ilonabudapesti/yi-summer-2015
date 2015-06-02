6fb8d668c5d498e832d2c3f9aef366e4623b5c55
Will Mainwaring <willm@yogainternational.com>
2015-06-02 16:05:59 -0400

    add start and end inputs

--- tracker app/js/main.js
+++ tracker app/js/main.js
@@ -1,17 +1,22 @@
-var entryArray = [];
+var entries = [];
 
  function addEntry() {
  	var entry = {};
 	entry.description = document.getElementById("test1").value;
 	entry.tags = document.getElementById("tags1").value;
-	entryArray.push(entry);
-	console.log(entryArray);
+	entry.start = document.getElementById("start").value;
+	entry.end = document.getElementById("end").value;
+	entries.push(entry);
+	console.log(entries);
 	var stringifyArray = JSON.stringify(entryArray);
 	var entriesContainer = document.getElementById("entriesContainer");
 	var p = document.createElement ('p');
 	var addArray = document.createTextNode(stringifyArray);
 	p.appendChild(addArray);
 	document.getElementById("entriesContainer").appendChild(p);
-	
+
 
 }
+
+
+
