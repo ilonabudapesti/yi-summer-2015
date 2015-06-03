fe1d0c440cc0c544e76701b79cd7571feaeb8443
Will Mainwaring <willm@yogainternational.com>
2015-06-02 23:58:23 -0400

    improved id naming in index.html and main.js

--- tracker app/js/main.js
+++ tracker app/js/main.js
@@ -3,15 +3,14 @@ var entries = [];
 
  function addEntry() {
  	var entry = {};
-	entry.description = document.getElementById("test1").value;
-	entry.tags = document.getElementById("tags1").value;
+	entry.description = document.getElementById("description").value;
+	entry.tags = document.getElementById("tags").value;
 	entry.start = document.getElementById("start").value;
 	entry.ends = document.getElementById("end").value;
 	entries.push(entry);
 		for (i; i < entries.length; i++) {	
-			var stringifyArray =  
-			entries[i].description + ", " + entries[i].tags+ ", " + entries[i].start + ", " +
-			entries[i].ends
+			var stringifyArray =  entries[i].description + ", " + entries[i].tags+ ", "
+			 + entries[i].start + ", " + entries[i].ends;
 			var entriesContainer = document.getElementById("entriesContainer");
 			var p = document.createElement ('p');
 			var addArray = document.createTextNode(stringifyArray);
