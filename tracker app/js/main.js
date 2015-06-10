d7fa5421a4bdbbab6aab329f922880ef9afc2bae
Will Mainwaring <willm@yogainternational.com>
2015-06-09 22:05:50 -0400

    refactor addEntry

--- tracker app/js/main.js
+++ tracker app/js/main.js
@@ -2,46 +2,47 @@ var entries = [];
  var i = 0;
 
  function addEntry() {
- 	var entry = {};
-	entry.description = document.getElementById("description").value;
-	entry.tags = document.getElementById("tags").value;
-	entry.start = document.getElementById("start").value;
-	entry.ends = document.getElementById("end").value;
-	entries.push(entry);
-		for (i; i < entries.length; i++) {	
-			/*var stringifyArray =  entries[i].description + ", " + entries[i].tags+ ", "
-			 + entries[i].start + ", " + entries[i].ends;
-			var entriesContainer = document.getElementById("entriesContainer");
-			var p = document.createElement ('p');
-			var addArray = document.createTextNode(stringifyArray);
-			p.appendChild(addArray);
-			document.getElementById("entriesContainer").appendChild(p);
-		*/
-		};
-	return i;
-
-}
-
-function generateTable() {
+  var entry = {};
+  entry.description = document.getElementById("description").value;
+  entry.tags = document.getElementById("tags").value;
+  entry.start = document.getElementById("start").value;
+  entry.ends = document.getElementById("end").value;
+  entries.push(entry);
+  for (i; i<entries.length; i++) {
   var tableDiv = document.getElementById("tableContainer");
   var tbl = document.createElement("table");
   tbl.className ="table";
   var tblBody = document.createElement("tbody");
   var row = document.createElement("tr");
-    for (key in entries[i-1]) {
+    for (key in entries[i]) {
       var cell = document.createElement("td");
       cell.setAttribute("padding","1em");
-      var cellText = document.createTextNode(entries[i-1][key]);
+      var cellText = document.createTextNode(entries[i][key]);
       cell.appendChild(cellText);
       row.appendChild(cell);
+      console.log (entries[i][key]);
    }
-    // add the row to the end of the table body
+  var deleteButton = document.createElement("button");
+  deleteButton.setAttribute("id","deleteButton");
+  deleteButton.setAttribute("value",'Delete Row');
+  var cell2 = document.createElement("td");
+  cell2.appendChild(deleteButton);
+  row.appendChild(cell2);
   tblBody.appendChild(row);
-  // put the <tbody> in the <table>
   tbl.appendChild(tblBody);
-  // appends <table> into <body>
   tableDiv.appendChild(tbl);
-  // sets the border attribute of tbl to 2;
   tbl.setAttribute("border", "1");
+  }
+  return i;
+
+  function deleteEntry() {
+  var setName = getElementById("deleteButton");
+
+
+
+
+
+}
+
 }
 
