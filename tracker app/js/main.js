315f34ffd19e9542b5d05edb4761560250539037
Will Mainwaring <willm@yogainternational.com>
2015-06-11 10:36:21 -0400

    add delete button and jquery library

--- tracker app/js/main.js
+++ tracker app/js/main.js
@@ -1,6 +1,5 @@
 var entries = [];
  var i = 0;
-
  function addEntry() {
   var entry = {};
   entry.description = document.getElementById("description").value;
@@ -8,41 +7,38 @@ var entries = [];
   entry.start = document.getElementById("start").value;
   entry.ends = document.getElementById("end").value;
   entries.push(entry);
-  for (i; i<entries.length; i++) {
-  var tableDiv = document.getElementById("tableContainer");
-  var tbl = document.createElement("table");
-  tbl.className ="table";
-  var tblBody = document.createElement("tbody");
-  var row = document.createElement("tr");
-    for (key in entries[i]) {
-      var cell = document.createElement("td");
-      cell.setAttribute("padding","1em");
-      var cellText = document.createTextNode(entries[i][key]);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
-      console.log (entries[i][key]);
-   }
-  var deleteButton = document.createElement("button");
-  deleteButton.setAttribute("id","deleteButton");
-  deleteButton.setAttribute("value",'Delete Row');
-  var cell2 = document.createElement("td");
-  cell2.appendChild(deleteButton);
-  row.appendChild(cell2);
-  tblBody.appendChild(row);
-  tbl.appendChild(tblBody);
-  tableDiv.appendChild(tbl);
-  tbl.setAttribute("border", "1");
-  }
-  return i;
-
-  function deleteEntry() {
-  var setName = getElementById("deleteButton");
-
-
-
-
-
+    for (i; i<entries.length; i++) {
+        var tableDiv = document.getElementById("entriesTable");
+        var tbl = document.createElement("table");
+        tbl.className ="table";
+        var tblBody = document.createElement("tbody");
+        var row = document.createElement("tr");
+            for (key in entries[i]) {
+                var cell = document.createElement("td");
+                cell.setAttribute("padding","1em");
+                var cellText = document.createTextNode(entries[i][key]);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+        var deleteButton = document.createElement("button");
+        deleteButton.setAttribute("id","deleteButton" + i);
+        deleteButton.setAttribute("value",'Delete Row');
+        var cell2 = document.createElement("td");
+        cell2.appendChild(deleteButton);
+        row.appendChild(cell2);
+        tblBody.appendChild(row);
+        tbl.appendChild(tblBody);
+        tableDiv.appendChild(tbl);
+        tbl.setAttribute("border", "1");
+        tbl.setAttribute("id", "row" + i)
+    }
+var k = i-1;
+ $( "#deleteButton"+ k).click(function() 
+  {
+    $('#row'+ k).remove();
+  });
+ return i;
 }
 
-}
+
 
