f77b1f90eae85a7eed1c7d522f9db9e5c4582bec
Ilona Budapesti <ilona.budapesti@gmail.com>
2015-06-16 20:29:02 -0400

    add rockPaperScissors coding challenge

---
+++ coding-challenges/rock-paper-scissors/rockPaperScissors.js
@@ -0,0 +1,26 @@
+/*
+* Imagine we are playing a three-round game of  rock, paper, scissors.
+* Write a function that outputs all possible combinations 
+* that one player could play.
+*
+* Your output should look something like:
+*   [["rock", "rock", "rock"],
+*    ["rock", "rock", "paper"],
+*    ["rock", "rock", "scissors"],
+*    ["rock", "paper", "rock"],
+             ...etc...
+     ]
+*
+* Extra credit:
+*   - Make your function return answers for any number of rounds.
+*   
+* Example:
+* rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
+*
+* Extra extra credit:
+*   - Write a test suite with mocha, add specs and a spec runner to test your function.
+*/
+
+var rockPaperScissors = function () {
+  // TODO: your solution here
+};
